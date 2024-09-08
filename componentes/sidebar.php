38725729dba9ead9d7fb265fda7990347edd869d
TheBest1959 <masterorigenmedios@gmail.com>
2024-09-08 14:31:24 +0000

    updateHeader

--- componentes/sidebar.php
+++ componentes/sidebar.php
@@ -2,8 +2,7 @@
         <aside id="sidebar-wrapper">
           <div class="sidebar-brand">
             <a href="dashboard"> 
-              <img alt="image" src="https://www.origenmedios.cl/wp-content/uploads/2023/10/logo-origen-2023-sm2.png" class="header-logo" />
-              <span class="logo-name"> <img alt="image" src="https://www.origenmedios.cl/wp-content/uploads/2023/10/logo-origen-2023-sm2.png" class="header-logo" /></span>
+              <img alt="image" src="https://ubiquitous-sniffle-wr9pjrwwp7w5hgpqp-3000.app.github.dev/assets/img/logo-origen-O.png" class="header-logo" /><img class="logo-name2" alt="image" src="https://ubiquitous-sniffle-wr9pjrwwp7w5hgpqp-3000.app.github.dev/assets/img/logo-origen-O3.png"/></span>
             </a>
           </div>
         
